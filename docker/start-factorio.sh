a6bf243f14cbc0d765ff7ed13389bca33c1fdfa2
LemonNeko <chheese048@gmail.com>
2025-08-20 21:27:09 +0800

    feat(realtime-vision): browser vnc viewer powered by noVNC

--- docker/start-factorio.sh
+++ docker/start-factorio.sh
@@ -7,6 +7,7 @@ while ! xdpyinfo -display $DISPLAY >/dev/null 2>&1; do
 done
 
 x11vnc -display $DISPLAY -nopw -forever &
+websockify --web=/usr/share/novnc/ 5901 localhost:5900 &
 
 ./factorio/bin/x64/factorio \
   --disable-audio \
