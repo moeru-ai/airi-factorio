e97c62d3a70e966b3fe39c84ba619d32e66412a1
LemonNeko <chheese048@gmail.com>
2025-08-22 01:43:21 +0800

    feat(realtime-vision): playground vnc support

--- docker/start-factorio.sh
+++ docker/start-factorio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Xvfb $DISPLAY -screen 0 1024x768x16 &
+Xvfb $DISPLAY -screen 0 640x640x16 &
 
 while ! xdpyinfo -display $DISPLAY >/dev/null 2>&1; do
   sleep 0.1
@@ -12,6 +12,6 @@ websockify --web=/usr/share/novnc/ 5901 localhost:5900 &
 ./factorio/bin/x64/factorio \
   --disable-audio \
   --force-graphics-preset very-low \
-  --window-size 1024x768 \
+  --window-size 640x640 \
   --graphics-quality low \
   --video-memory-usage low
